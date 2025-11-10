592efd77f230b22496f05f843bf0504ff53fc4ea
Sasha0119 <shohjahon.baxtiyorov.11@gmail.com>
2025-11-10 16:19:01 +0500

    Change logo and update App component content
    Updated logo import and modified the content in the App component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './mansur.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Salom </h1>
+        <p> Men mansur reactni o'rganmoqda </p>
+        <a className="App-link" target="_blank" rel="noopener noreferrer"></a>
       </header>
     </div>
   );
